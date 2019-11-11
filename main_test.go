def43ca1ecba61e6d05201758d428bc9e824510e
Julen Dixneuf <julen@loudnaround.org>
2019-11-11 21:46:04 +0100

    add tests

--- main_test.go
+++ main_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
+
 	"testing"
 
 	"github.com/dixneuf19/go-crud-api/greetings"
@@ -36,8 +38,16 @@ func testHandlerFunc(t *testing.T, handlerFunc http.HandlerFunc, params testRequ
 }
 
 func testHandler(t *testing.T, handler http.Handler, params testRequestParams) {
-	req := httptest.NewRequest(params.Method, params.Path, nil)
+	req := httptest.NewRequest(params.Method, params.Path, strings.NewReader(params.Body))
+	// req := httptest.NewRequest(params.Method, params.Path, nil)
+
 	w := httptest.NewRecorder()
+
+	for key, values := range params.Headers {
+		for _, value := range values {
+			req.Header.Add(key, value)
+		}
+	}
 
 	handler.ServeHTTP(w, req)
 
@@ -73,11 +83,14 @@ func TestHelloHandlerFunc(t *testing.T) {
 
 }
 
-func TestGetGreetingsHandler(t *testing.T) {
+func TestGreetingsHandler(t *testing.T) {
 
 	gp := greetings.NewGreetingsMap()
 	gp.Add("en", "hello")
 	gp.Add("fr", "bonjour")
+
+	header := http.Header{}
+	header.Add("Content-Type", "application/json")
 
 	requests := []testRequestParams{
 		{
@@ -91,6 +104,50 @@ func TestGetGreetingsHandler(t *testing.T) {
 			Path:    "/hello?language=en",
 			ExpCode: http.StatusBadRequest,
 			ExpMsg:  "Please provide a language as 'lang' query parameter. Ex: /hello?lang=en",
+		},
+		{
+			Method:  http.MethodGet,
+			Path:    "/hello?lang=en",
+			ExpCode: http.StatusOK,
+			ExpMsg:  "hello",
+		},
+		{
+			Method:  http.MethodGet,
+			Path:    "/hello?lang=fr",
+			ExpCode: http.StatusOK,
+			ExpMsg:  "bonjour",
+		},
+		{
+			Method:  http.MethodGet,
+			Path:    "/hello?lang=it",
+			ExpCode: http.StatusBadRequest,
+			ExpMsg:  "I don't know how to greet in 'it'. Learn me how with a POST method",
+		},
+		{
+			Method:  http.MethodPost,
+			Path:    "/hello?lang=it",
+			Headers: header,
+			Body:    `{"language": "it", "hello": "buongiorno"}`,
+			ExpCode: http.StatusCreated,
+			ExpMsg:  "",
+		},
+		{
+			Method:  http.MethodGet,
+			Path:    "/hello?lang=it",
+			ExpCode: http.StatusOK,
+			ExpMsg:  "buongiorno",
+		},
+		{
+			Method:  http.MethodDelete,
+			Path:    "/hello?lang=it",
+			ExpCode: http.StatusOK,
+			ExpMsg:  "",
+		},
+		{
+			Method:  http.MethodGet,
+			Path:    "/hello?lang=it",
+			ExpCode: http.StatusBadRequest,
+			ExpMsg:  "I don't know how to greet in 'it'. Learn me how with a POST method",
 		},
 	}
 
